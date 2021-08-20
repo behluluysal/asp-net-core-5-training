fc4dc491c92cd98e754451daeb4456726756a456
Behlül UYSAL <mockingjaytes@gmail.com>
2021-08-20 17:16:11 +0300

    Initialized project

---
+++ rokWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace rokWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
